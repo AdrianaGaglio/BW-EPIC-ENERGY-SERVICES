67fd3fb49ba5d661c12a752360dd744620c777a5
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-01-20 12:27:00 +0100

    Initialized Auth

--- be/src/main/java/epicode/it/energyservices/auth/AppUser.java
+++ be/src/main/java/epicode/it/energyservices/auth/AppUser.java
@@ -1,4 +1,34 @@
 package epicode.it.energyservices.auth;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    private String name;
+    private String surname;
+    private String avatar;
+
+//    @OneToOne
+//    private SysUser sysUser;
 }
---
+++ be/src/main/java/epicode/it/energyservices/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package epicode.it.energyservices.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
