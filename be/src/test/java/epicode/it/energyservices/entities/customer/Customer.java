c9ac58d359e3fc1d3de020bba866f12e196ee677
Adriana Gaglio <adriana.gaglio@gmail.com>
2025-01-20 10:58:19 +0100

    folder tree

--- be/src/test/java/epicode/it/energyservices/entities/customer/Customer.java
+++ be/src/test/java/epicode/it/energyservices/entities/customer/Customer.java
@@ -1,4 +1,15 @@
 package epicode.it.energyservices.entities.customer;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private  Long id;
+
+
 }
