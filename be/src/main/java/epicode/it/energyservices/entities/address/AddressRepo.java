0acc3ad6a3807c498754874d62137845d3325fd9
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-01-21 16:30:44 +0100

    add preauthorize to addresscontroller

--- be/src/main/java/epicode/it/energyservices/entities/address/AddressRepo.java
+++ be/src/main/java/epicode/it/energyservices/entities/address/AddressRepo.java
@@ -2,5 +2,8 @@ package epicode.it.energyservices.entities.address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AddressRepo extends JpaRepository<Address, Long> {
+    List<Address> findAllByCustomerId(Long id);
 }
