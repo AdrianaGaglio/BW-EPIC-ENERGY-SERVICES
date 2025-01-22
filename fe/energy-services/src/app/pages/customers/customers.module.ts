4f0881c4a0b4774d363a1bee67f6666b8806cd52
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-01-22 12:46:32 +0100

    Wip customers component

--- fe/energy-services/src/app/pages/customers/customers.module.ts
+++ fe/energy-services/src/app/pages/customers/customers.module.ts
@@ -6,13 +6,16 @@ import { CustomersComponent } from './customers.component';
 import { LoggedCustomersComponent } from './components/logged-customers/logged-customers.component';
 import { ForadminuserComponent } from './components/foradminuser/foradminuser.component';
 import { FormsModule } from '@angular/forms';
+import { SearchByCreationdateComponent } from './components/search-by-creationdate/search-by-creationdate.component';
+import { NgIconsModule } from '@ng-icons/core';
 
 @NgModule({
   declarations: [
     CustomersComponent,
     LoggedCustomersComponent,
     ForadminuserComponent,
+    SearchByCreationdateComponent,
   ],
-  imports: [CommonModule, CustomersRoutingModule, FormsModule],
+  imports: [CommonModule, CustomersRoutingModule, FormsModule, NgIconsModule],
 })
 export class CustomersModule {}
