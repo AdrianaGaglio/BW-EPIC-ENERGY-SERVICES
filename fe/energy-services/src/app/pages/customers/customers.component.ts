57e9f2eb258936c03baa1529e2047bcdec821516
MarcoCipolletta <marcocipolletta1998@gmail.com>
2025-01-22 17:10:04 +0100

    add refresh roles at login

--- fe/energy-services/src/app/pages/customers/customers.component.ts
+++ fe/energy-services/src/app/pages/customers/customers.component.ts
@@ -13,16 +13,18 @@ export class CustomersComponent {
   constructor(
     private customerSvc: CustomerService,
     private decodeToken: DecodeTokenService
-  ) {}
-  roles: string[] = [];
-  isCustomer: boolean = false;
-
-  ngOnInit(): void {
+  ) {
     this.roles = this.decodeToken.userRoles$.getValue();
     console.log(this.roles);
 
     if (this.roles.includes('CUSTOMER')) {
       this.isCustomer = true;
+    } else {
+      this.isCustomer = false;
     }
   }
+  roles: string[] = [];
+  isCustomer: boolean = false;
+
+  ngOnInit(): void {}
 }
--- fe/energy-services/src/app/services/decode-token.service.ts
+++ fe/energy-services/src/app/services/decode-token.service.ts
@@ -7,6 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class DecodeTokenService {
   constructor() {
+    console.log('decode token service');
     this.userRoles$.next(this.getRoles());
   }
 
