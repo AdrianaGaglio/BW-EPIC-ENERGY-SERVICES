8c613a8dedc9c259b5df6d4bcf2cbdbe492c6cff
Adriana Gaglio <adriana.gaglio@gmail.com>
2025-01-23 15:02:09 +0100

    wip guards

--- fe/energy-services/src/app/guards/guest.guard.ts
+++ fe/energy-services/src/app/guards/guest.guard.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  GuardResult,
+  MaybeAsync,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthsrvService } from '../auth/authsrv.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GuestGuard implements CanActivate, CanActivateChild {
+  constructor(private authSvc: AuthsrvService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): MaybeAsync<GuardResult> {
+    state: RouterStateSnapshot
+  ): MaybeAsync<GuardResult> {
+    if (this.authSvc.userAuthSubject$.getValue() != null) {
+      return false;
+    }
     return true;
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    return true;
+    state: RouterStateSnapshot
+  ): MaybeAsync<GuardResult> {
+    return this.canActivate(childRoute, state);
   }
-  
 }
